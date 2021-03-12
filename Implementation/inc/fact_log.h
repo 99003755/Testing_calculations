815cb173c0b71667ef9f15078f396f3b2933d47f
99003746 <nitin.shetty1@ltts.com>
2021-03-12 09:21:58 +0530

    updated

---
+++ Implementation/inc/fact_log.h
@@ -0,0 +1,11 @@
+//#ifndef __FACTORIAL_LOGARITHM_H_
+//#define __FACTORIAL_LOGARITHM_H_
+
+#include <stdio.h> 
+#include <math.h>
+
+float product(float a, float b);
+float ratios(float a, float b) ;
+int factorial(int n);
+
+//#endif 
